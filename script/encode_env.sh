e4f7bdfc6bd682d2e46ae1ba0dc4f7ba0e5fa244
Hoàng Nguyễn <hoang.nguyen@journeyh.io>
2021-04-19 16:40:10 +0700

    Add get env

--- script/encode_env.sh
+++ script/encode_env.sh
@@ -2,4 +2,30 @@
 
 set -e
 
-echo "current branch: $CIRCLE_BRANCH";
+source ./scripts/set_environment.sh
+
+echo "npm-debug.log\nyarn-error.log" > .dockerignore
+export AWS_PRIVATE_KEY_PATH='permission.pem'
+cp Dockerfile_ci Dockerfile
+
+if [[ "$CIRCLE_BRANCH" == "master" ]] || [[ "$CIRCLE_BRANCH" == "production" ]] ; then
+  # todo: set up aws credentials for getting the env file
+  mkdir ~/.aws
+  touch ~/.aws/config
+  touch ~/.aws/credentials
+
+  echo "[profile environment]" >> ~/.aws/config
+  echo "[environment]" >> ~/.aws/credentials
+  echo "aws_access_key_id = ${AWS_ENV_USER_ACCESS_KEY_ID}" >> ~/.aws/credentials
+  echo "aws_secret_access_key = ${AWS_ENV_USER_SECRET_ACCESS_KEY}" >> ~/.aws/credentials
+
+  aws secretsmanager get-secret-value --secret-id ${AWS_ENV_SECRET_NAME} --query SecretString --output text --profile=environment > .env.json
+  ./scripts/json2env.sh .env.json .env
+
+  cat .env
+  echo "---------------\n"
+  cat ~/.aws/credentials
+  echo "---------------\n"
+  cat ~/.aws/config
+
+fi
---
+++ script/set_environment.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -e
+
+export export AWS_ENV_SECRET_NAME='web/test/tutorkoala'
